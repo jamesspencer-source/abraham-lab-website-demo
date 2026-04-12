020a0103296bb2296856a576257926ed5ef0fb4f
jamesspencer-source <jmspencer1996@gmail.com>
2026-04-12 16:34:03 -0400

    Polish Abraham Lab layout and homepage location

--- src/_data/site.js
+++ src/_data/site.js
@@ -9,7 +9,7 @@ module.exports = {
   heroTitle:
     "Understanding how viruses enter cells, evade antibodies, and replicate.",
   heroDeck:
-    "The Abraham Lab uses molecular virology and structural biology to study receptor recognition, antibody escape, and replication machinery in medically important viruses, with an emphasis on emerging pathogens and human disease.",
+    "The Abraham Lab uses molecular virology and structural biology to define receptor recognition, antibody escape, and replication mechanisms in medically important viruses, with an emphasis on emerging pathogens and human disease.",
   heroDetails: [
     "Viral entry and receptor usage",
     "Antibody-mediated neutralization and escape",
@@ -17,6 +17,20 @@ module.exports = {
   ],
   heroApproach:
     "Structural biology, molecular virology, protein biochemistry, genetics, and infection-focused functional assays.",
+  heroDossier: [
+    {
+      label: "Institution",
+      value: "Department of Microbiology, Blavatnik Institute, Harvard Medical School"
+    },
+    {
+      label: "Focus",
+      value: "Viral entry, antibody escape, and replication machinery"
+    },
+    {
+      label: "Methods",
+      value: "Structural biology, molecular virology, and biophysics"
+    }
+  ],
   institutionLabel: "Institutional Affiliation",
   institutionTitle: "Department of Microbiology, Blavatnik Institute, Harvard Medical School",
   institutionSummary:
@@ -47,7 +61,7 @@ module.exports = {
       href: "https://www.hhmi.org/",
       logo: "/assets/images/brands/hhmi-horizontal-signature-color.png",
       alt: "Howard Hughes Medical Institute",
-      note: "Investigator affiliation"
+      note: "HHMI Investigator"
     }
   ],
   contact: {
@@ -56,10 +70,21 @@ module.exports = {
     addressLines: ["77 Avenue Louis Pasteur", "NRB 939", "Boston, MA 02115"],
     email: "jonathan_abraham@hms.harvard.edu",
     managerEmail: "james_spencer@hms.harvard.edu",
+    mapDisplayName: "Harvard Medical School",
+    homeLocationLabel: "Harvard Medical School",
+    mapQuery: "Harvard Medical School, 77 Avenue Louis Pasteur, Boston, MA 02115",
     mapUrl:
-      "https://www.google.com/maps/search/77+Avenue+Louis+Pasteur+Boston+MA+02115",
+      "https://www.google.com/maps/search/?api=1&query=Harvard+Medical+School%2C+77+Avenue+Louis+Pasteur%2C+Boston%2C+MA+02115",
     mapEmbedUrl:
-      "https://www.google.com/maps?q=77+Avenue+Louis+Pasteur,+Boston,+MA+02115&z=16&output=embed"
+      "https://www.google.com/maps?q=Harvard+Medical+School,+77+Avenue+Louis+Pasteur,+Boston,+MA+02115&z=16&output=embed",
+    mapPreviewTiles: [
+      "/assets/images/location/hms-map-tile-1.png",
+      "/assets/images/location/hms-map-tile-2.png",
+      "/assets/images/location/hms-map-tile-3.png",
+      "/assets/images/location/hms-map-tile-4.png"
+    ],
+    mapPreviewAlt:
+      "Map preview centered on Harvard Medical School in the Longwood Medical Area of Boston."
   },
   social: {
     x: "https://twitter.com/abrahamlabhms",
